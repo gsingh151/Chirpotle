b7fddd0fa9fe052d63cb8b32d9de630b0216bf3d
gsingh151 <45342803+gsingh151@users.noreply.github.com>
2022-04-06 01:18:00 -0400

    added db connection

--- index.php
+++ index.php
@@ -13,25 +13,43 @@
 
 </div>
 
-<form class="register">
-  <input type="text" name="username" placeholder="username" required autocomplete="off">
-  <input type="text" name="first_name" placeholder="first name" required autocomplete="off">
-  <input type="text" name="last_name" placeholder="last name" required autocomplete="off">
-  <input type="text" name="email" placeholder="email" required autocomplete="off">
-  <input type="text" name="password" placeholder="password" required autocomplete="off">
+<form class="register" method="POST">
+  <input type="text" name="username" placeholder="username" required>
+  <input type="text" name="first_name" placeholder="first name" required>
+  <input type="text" name="last_name" placeholder="last name" required>
+  <input type="text" name="email" placeholder="email" required>
+  <input type="text" name="password" placeholder="password" required>
   <input type="submit" name="register" value="SIGN UP">
 </form>
 
 <?php
-if (isset($_GET['username'])){
-  $wallet = $_GET['username'];
-  echo $wallet;
-}else{
-  echo "it's not set";
+if (isset($_POST['register'])){
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "db_chirpotle";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+  $username = $_POST['username'];
+  $first_name = $_POST['first_name'];
+  $last_name = $_POST['last_name'];
+  $email = $_POST['email'];
+  $password = md5($_POST['password']);
+  $sign_up_date = date('Y-m-d');
+
+  $verify = mysqli_query($conn, "SELECT * FROM `users` WHERE `email` = '$email'");
+  if ($mysqli_num_rows($verify) > 0){
+    echo "this user exists";
+  }else{
+    echo "this user does not exist";
+    $sql = "INSERT INTO `users`(`username`, `password`, `first_name`, `last_name`, `email`, `sign_up_date`) VALUES ('$username', '$password', '$first_name', '$last_name', '$email', '$sign_up_date')";
+    $query = mysqli_query($conn, $sql);
+    echo "Username and Password Submitted!";
+  }
 }
 ?>
-
-
 
 </div>
 <?php include 'inc/footer.php' ?>
